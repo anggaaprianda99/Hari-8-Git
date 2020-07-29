e1d17c64ad157a43dc10a10fadbbbc4999e2c574
anggaaprianda99 <anggaaprianda04@gmail.com>
2020-07-30 01:39:24 +0700

    Menambahkan semua file

---
+++ ubah-huruf.php
@@ -0,0 +1,22 @@
+<?php
+function ubah_huruf($string){
+    $abjad = "abcdefghijklmnopqrstuvwxyz";
+    $hasil = "";
+    for($i = 0;$i < strlen($string);$i++){
+        $posisi = strpos($abjad, $string[$i]);
+        $hasil .= substr($abjad, $posisi+1,1); 
+    }
+    return $hasil;
+}
+
+// TEST CASES
+echo ubah_huruf('wow'); // xpx
+echo "<br>";
+echo ubah_huruf('developer'); // efwfmpqfs
+echo "<br>";
+echo ubah_huruf('laravel'); // mbsbwfm
+echo "<br>";
+echo ubah_huruf('keren'); // lfsfo
+echo "<br>";
+echo ubah_huruf('semangat'); // tfnbohbu
+?>
