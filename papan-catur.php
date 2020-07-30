737cf2fd1905bdd97a1dd593e52438a953c19640
anggaaprianda99 <anggaaprianda04@gmail.com>
2020-07-30 23:59:57 +0700

    Update papan catur

--- papan-catur.php
+++ papan-catur.php
@@ -1,36 +1,27 @@
 <?php
 
-for($i=0;$i < 5;$i++){
-    for($j=0;$j <$i;$j++){
-        echo "# <br>";
+function papan_catur($angka) {
+// tulis kode di sini
+    for($i = 0; $i <$angka; $i++){
+        if($i % 2 == 0){
+            echo "";
+            for($j=0;$j <$angka-1;$j++){
+                echo "&nbsp;#";
+            }
+        }else{
+            for($j=0;$j<$angka-1;$j++){
+                echo "#&nbsp";
+            }
+        }
+        echo "<br>";
     }
 }
 
-
-
 // TEST CASES
-/*
-# # # #
- # # #
-# # # #
- # # #
- */
+echo papan_catur(4) . "<br>"; 
 
-/* 
-# # # # # # # #
- # # # # # # # 
-# # # # # # # #
- # # # # # # # 
-# # # # # # # #
- # # # # # # #
-# # # # # # # #
- # # # # # # #
+echo "<br>". papan_catur(8);
 
-echo papan_catur(5) 
-/*
-# # # # #
- # # # #
-# # # # #
- # # # # 
-# # # # #
-*/
+echo "<br>". papan_catur(5); 
+
+?>
